074f18ec6379788970b9a768f486543f3e6a7f5c
Shimaa Nashat Abdelmageed <shimaanashat78@gmail.com>
2026-02-07 11:06:00 +0200

    Move main execution block to the end of app.py

--- app.py
+++ app.py
@@ -36,9 +36,6 @@ def index():
     return render_template("index.html", username=session.get("username"))
 
 # ---------- Auth ----------
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8080))
-    app.run(host="0.0.0.0", port=port)
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
@@ -190,3 +187,8 @@ def contact():
         return redirect("/contact")
 
     return render_template("contact.html")
+
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
